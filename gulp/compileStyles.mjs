a87689a1a819d4138d99f2cf38e4f7c02f98ff2e
Roman Grachev <romagra92@gmail.com>
2023-04-04 23:16:47 +0500

    Добавил шапку и главный экран

--- gulp/compileStyles.mjs
+++ gulp/compileStyles.mjs
@@ -4,7 +4,7 @@ import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
 import autoprefixer from 'autoprefixer';
 import csso from 'gulp-csso';
-import gcmq from 'gulp-group-css-media-queries';
+// import gcmq from 'gulp-group-css-media-queries';
 import rename from 'gulp-rename';
 
 const sass = gulpSass(dartSass);
@@ -15,7 +15,7 @@ const compileStyles = () =>
       .pipe(postcss([autoprefixer({
         grid: true,
       })]))
-      .pipe(gcmq()) // выключите, если в проект импортятся шрифты через ссылку на внешний источник
+      // .pipe(gcmq()) // выключите, если в проект импортятся шрифты через ссылку на внешний источник
       .pipe(gulp.dest('build/css'))
       .pipe(csso())
       .pipe(rename('style.min.css'))
--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
+import './modules/burger/index';
+
 
 // ---------------------------------
 
