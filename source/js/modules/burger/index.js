3fbd0ca884f7d1f912434c6ee0fc6d39b585683d
Roman Grachev <romagra92@gmail.com>
2023-04-06 20:26:59 +0500

    Добавил блок преимуществ

--- source/js/modules/burger/index.js
+++ source/js/modules/burger/index.js
@@ -1,6 +1,4 @@
-import './locomotive-scroll';
 import {Burger} from './burger';
-import {StickyHeader} from './sticky-header';
 import {initLocomotiveScroll} from './init-locomotive-scroll';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -9,7 +7,5 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener('load', () => {
     const burger = new Burger();
     burger.init();
-    const stickyHeader = new StickyHeader();
-    stickyHeader.init();
   });
 });
